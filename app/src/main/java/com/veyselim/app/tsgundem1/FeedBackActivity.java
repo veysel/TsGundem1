c6b9f8de1cab80c40e816a186bb060012f597eed
veysel <vinanc72@gmail.com>
2018-10-17 12:37:25 +0300

    Feedback activity created

---
+++ app/src/main/java/com/veyselim/app/tsgundem1/FeedBackActivity.java
@@ -0,0 +1,13 @@
+package com.veyselim.app.tsgundem1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FeedBackActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed_back);
+    }
+}
